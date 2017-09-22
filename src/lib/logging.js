470485f952c7d318dd6bf9da4d2bda91840b4c60
Alex <alex.lee3851@gmail.com>
2017-09-22 15:08:53 -0700

    Colored console logs

--- src/lib/logging.js
+++ src/lib/logging.js
@@ -1,11 +1,20 @@
 /* eslint-disable no-console */
 const env = process.env.NODE_ENV || 'development';
+const colorizeEnv = () => {
+  if (env === 'production') {
+    return `\x1b[32m(${env})\x1b[0m`; // green
+  }
+  return `\x1b[36m(${env})\x1b[0m`; // cyan
+};
+const colorizeTimestamp = () => { // yellow
+  const time = new Date().toLocaleString();
+  return `\x1b[33m[${time}]\x1b[0m`;
+};
 
 export default {
   system: (...args) => {
     // Always print
-    const date = new Date();
-    console.log(`[${env}][${date.toLocaleString()}] -`, ...args);
+    console.log(`${colorizeEnv()} ${colorizeTimestamp()} -`, ...args);
   },
   log: (...args) => {
     if (env !== 'production') {
